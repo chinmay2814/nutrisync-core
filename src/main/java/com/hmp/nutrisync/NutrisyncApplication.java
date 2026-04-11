07b5dce44ff443eb77193fa37ded4217cb3fbcc8
Keshav1707 <keshavdewaker@gmail.com>
2026-04-12 02:35:50 +0530

    project init

---
+++ src/main/java/com/hmp/nutrisync/NutrisyncApplication.java
@@ -0,0 +1,13 @@
+package com.hmp.nutrisync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrisyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrisyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hmp/nutrisync/NutrisyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hmp.nutrisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrisyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
